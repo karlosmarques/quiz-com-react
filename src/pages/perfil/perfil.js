2bea0306f8bc66850dbebe85117d7647483dd2fc
karlos marques de menezes <103792595+karlosmarques@users.noreply.github.com>
2025-05-16 09:15:44 -0300

    Update perfil.js

--- src/pages/perfil/perfil.js
+++ src/pages/perfil/perfil.js
@@ -1,6 +1,8 @@
-import React from 'react';
 
-
+import axios from "axios";
+import { jwtDecode } from "jwt-decode";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Perfil = () => {
     return (
